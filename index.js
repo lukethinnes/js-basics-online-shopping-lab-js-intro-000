e1aee96da11113993381990d4dddb7f620746e72
Luke Thinnes <lukethinnes@yahoo.com>
2020-10-20 02:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function viewCart() {
 
 function total() {
   let numbers = []
-  let sum = 0 
+  let sum = 0
   for (var i = 0; i < cart.length; i++) {
     numbers.push(cart[i].itemPrice)
     sum += numbers[i]
